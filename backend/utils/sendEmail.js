4d53ad36f2c100dd2295e886468c21cc9ec3a171
unknown <mrittunjoysaha25@gmail.com>
2026-07-27 19:52:33 +0530

    Fix sendEmail syntax

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,30 +1,34 @@
-const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const nodemailer = require("nodemailer");
 
-await transporter.verify();
-console.log("✅ SMTP connection successful");
+const sendEmail = async (to, subject, text) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp-relay.brevo.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-const info = await transporter.sendMail({
-  from: '"EventSync" <mrittunjoysaha25@gmail.com>',
-  to,
-  subject,
-  text,
-});
+    await transporter.verify();
+    console.log("✅ SMTP connection successful");
 
-console.log("✅ Email sent:", info.response);
+    const info = await transporter.sendMail({
+      from: '"EventSync" <mrittunjoysaha25@gmail.com>',
+      to,
+      subject,
+      text,
+    });
 
-return info;
+    console.log("✅ Email sent:", info.response);
+
+    return info;
   } catch (err) {
-  console.error("❌ Email Error:");
-  console.error(err);
-}
+    console.error("❌ Email Error:");
+    console.error(err);
+  }
 };
 
 module.exports = sendEmail;
