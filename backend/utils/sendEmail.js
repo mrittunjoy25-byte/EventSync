14b2b514231cbf954e817f25f47843d5df586d8a
unknown <mrittunjoysaha25@gmail.com>
2026-07-27 19:00:09 +0530

    Switch to Brevo SMTP

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,14 +3,12 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (to, subject, text) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      requireTLS: true,
-      family: 4,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
       },
       connectionTimeout: 30000,
       greetingTimeout: 30000,
